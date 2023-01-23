04c76dbb3377acd8e5fdc3ac230c449bd21ac7de
MoritzHein <moritz.heinlein@tu-dortmund.de>
2023-01-23 16:24:29 +0100

    Upload CSTR-Cascade-Model
    CSTR-Cascade Model and basic MPC included

---
+++ 01_Example_Systems/CSTR-Cascade/CSTRcontrol.py
@@ -0,0 +1,163 @@
+# %%
+# Essentials
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import sys
+
+# Tools
+from IPython.display import clear_output
+import copy
+import sys
+import importlib
+
+# Typing information
+from typing import Tuple, List, Dict, Union, Optional, Any
+
+# Specialized packages
+from casadi import *
+from casadi.tools import *
+
+# Control packages
+sys.path.append(os.path.join('..', '..', '..', 'do-mpc'))
+import do_mpc
+
+# Model
+import CSTRmodel
+# %%
+# Generate simulator for the CSTR model
+def get_simulator(t_step: float, CSTR: CSTRmodel.CSTR_Cascade) -> do_mpc.simulator.Simulator:
+    """
+    Create a simulator for the quadcopter model.
+    """
+    simulator = do_mpc.simulator.Simulator(CSTR.model)
+    simulator.set_param(t_step = t_step)
+
+    # Pass dummy tvp fCSTR.n_reaction (these wont affect the simulation)
+    sim_tvp_template = simulator.get_tvp_template()
+    simulator.set_tvp_fun(lambda t: sim_tvp_template)
+
+    simulator.setup()
+
+    return simulator
+
+# %%
+def get_MPC(t_step: float, CSTR: CSTRmodel.CSTR_Cascade) -> Tuple[do_mpc.controller.MPC, Any]:
+    mpc = do_mpc.controller.MPC(CSTR.model)
+
+    setup_mpc = {
+        'n_horizon': 35,
+        't_step': t_step,   
+        'store_full_solution': True,
+        # Use MA27 linear solver in ipopt for faster calculations:
+        'nlpsol_opts': {'ipopt.linear_solver': 'mumps'}
+    }
+    mpc.set_param(**setup_mpc)
+
+    surpress_ipopt = {'ipopt.print_level':0, 'ipopt.sb': 'yes', 'print_time':0}
+    mpc.set_param(nlpsol_opts = surpress_ipopt)
+
+    mpc_tvp_template = mpc.get_tvp_template()
+    def mpc_tvp_fun(t_now):
+        return mpc_tvp_template
+    mpc.set_tvp_fun(mpc_tvp_fun)
+
+    # Constraints
+    lb_x=0*np.ones((CSTR.nx,1))
+    ub_x=4*np.ones((CSTR.nx,1))
+    # state constraints
+    lb_cA=0
+    ub_cA=4
+    lb_cB=0
+    ub_cB=4
+    lb_cR=0
+    ub_cR=4
+    lb_cS=0
+    ub_cS=0.12
+    lb_Tr=20
+    ub_Tr=80
+
+
+    lb_x[:CSTR.n_reac]=lb_cA
+    ub_x[:CSTR.n_reac]=ub_cA
+    lb_x[CSTR.n_reac:2*CSTR.n_reac]=lb_cB
+    ub_x[CSTR.n_reac:2*CSTR.n_reac]=ub_cB
+    lb_x[2*CSTR.n_reac:3*CSTR.n_reac]=lb_cR
+    ub_x[2*CSTR.n_reac:3*CSTR.n_reac]=ub_cR
+    lb_x[3*CSTR.n_reac:4*CSTR.n_reac]=lb_cS
+    ub_x[3*CSTR.n_reac:4*CSTR.n_reac]=ub_cS
+    lb_x[4*CSTR.n_reac:]=lb_Tr
+    ub_x[4*CSTR.n_reac:]=ub_Tr
+    # input constraints
+    lb_uA = 0
+    ub_uA = 1.5
+    lb_uB = 0
+    ub_uB = 1.5
+    lb_Tj = 20
+    ub_Tj = 80
+    lb_u=0*np.ones((CSTR.nu,1))
+    ub_u=np.inf*np.ones((CSTR.nu,1))
+    lb_u[:CSTR.n_reac]=lb_uA
+    ub_u[:CSTR.n_reac]=ub_uA
+    lb_u[CSTR.n_reac:2*CSTR.n_reac]=lb_uB
+    ub_u[CSTR.n_reac:2*CSTR.n_reac]=ub_uB
+    lb_u[2*CSTR.n_reac:]=lb_Tj
+    ub_u[2*CSTR.n_reac:]=ub_Tj
+
+
+    QS = 0.5
+    QS = QS*np.diag(np.ones(CSTR.n_reac))
+    QS[-1,-1]*CSTR.n_reac
+    QA = 0
+    QA = QA*np.diag(np.ones(CSTR.n_reac))
+    #QA[-1,-1]CSTR.n_reac
+    QB = 0
+    QB = QB*np.diag(np.ones(CSTR.n_reac))
+    #QB[-1,-1]CSTR.n_reac
+    QR = 1
+    QR = QR*np.diag(np.ones(CSTR.n_reac))
+    QR[-1,-1]*CSTR.n_reac
+    #print(Q)
+    R_c = 0.001
+    R_c = R_c*np.diag(np.ones(CSTR.nu))
+    for i in range(CSTR.n_reac):
+        R_c[-i,-i]=R_c[-i,-i]/(60**2/(1.5**2))
+
+    lterm = 0 
+    lterm += CSTR.model.x['cS'].T@QS@CSTR.model.x['cS']+CSTR.model.x['cA'].T@QA@CSTR.model.x['cA']+CSTR.model.x['cB'].T@QB@CSTR.model.x['cB']+(CSTR.model.x['cR']-1.5).T@QR@(CSTR.model.x['cR']-1.5)
+    lterm +=(sum1(CSTR.model.u['uA'])-ub_u[0])@(sum1(CSTR.model.u['uA'])-ub_u[0])
+    lterm +=(sum1(CSTR.model.u['uB'])-ub_u[CSTR.n_reac])@(sum1(CSTR.model.u['uB'])-ub_u[CSTR.n_reac])
+    lterm +=(0.0001/(ub_Tj-lb_Tj)**2)*(CSTR.model.u['Tj']-lb_Tj).T@(CSTR.model.u['Tj']-lb_Tj)
+
+    mterm = DM.zeros((1,1))
+    mpc.set_objective(lterm=lterm, mterm=mterm)
+    mpc.set_rterm(uA=0.001)
+    mpc.set_rterm(uB=0.001)
+    mpc.set_rterm(Tj=0.001/(60**2/(1.5**2)))
+
+    mpc.bounds['lower','_u','uA'] = lb_uA
+    mpc.bounds['lower','_u','uB'] = lb_uB
+    mpc.bounds['lower','_u','Tj'] = lb_Tj
+    mpc.bounds['upper','_u','uA'] = ub_uA
+    mpc.bounds['upper','_u','uB'] = ub_uB
+    mpc.bounds['upper','_u','Tj'] = ub_Tj
+
+    mpc.bounds['lower','_x','cA']=lb_cA
+    mpc.bounds['lower','_x','cB']=lb_cB
+    mpc.bounds['lower','_x','cR']=lb_cR
+    mpc.bounds['lower','_x','cS']=lb_cS
+    mpc.bounds['lower','_x','Tr']=lb_Tr
+    mpc.bounds['upper','_x','cA']=ub_cA
+    mpc.bounds['upper','_x','cB']=ub_cB
+    mpc.bounds['upper','_x','cR']=ub_cR
+    mpc.bounds['upper','_x','cS']=ub_cS
+    mpc.bounds['upper','_x','Tr']=ub_Tr
+
+    mpc.setup()
+    x_0=0*np.ones((CSTR.nx,1))
+    x_0[-CSTR.n_reac:]=CSTR.Tr_in
+    mpc.x0 = x_0
+    mpc.set_initial_guess()
+
+    return mpc, mpc_tvp_template
---
+++ 01_Example_Systems/CSTR-Cascade/CSTRmodel.py
@@ -0,0 +1,98 @@
+import numpy as np
+from casadi import *
+import do_mpc
+
+class CSTR_Cascade:
+    """Baseline model for a quadcopter.
+    The model represents a Crazyflie 2.0 quadcopter. See datasheet:
+    https://www.bitcraze.io/documentation/hardware/crazyflie_2_0/crazyflie_2_0-datasheet.pdf
+
+    Inertia, mass and linear thrust taken from:
+    https://www.research-collection.ethz.ch/handle/20.500.11850/214143
+    """
+    def __init__(self,n_reac=3):
+        # Parameters
+        self.n_reac=n_reac
+        self.nx = 5*n_reac # 4 concentrations and 1 temperature each Reactor
+        self.nu = 3*n_reac # Input of A+B+T_jacket
+        self.np = 4*n_reac # 2 reaction rates and two heat of reactions each reactor, modelled independently due to e.g. fouling
+
+        self.V_R=15
+        self.V_i=self.V_R/n_reac
+        self.V_out=1
+        self.k1_0=2
+        self.k2_0=2
+        self.delH1_0=-100
+        self.delH2_0=-50
+        self.Tr_in=60
+        self.kA=2
+        self.rho=1
+        self.cp=0.5
+        self.Ea1=500
+        self.Ea2=600
+        self.R_gas=8.3145
+
+        # Initialize model
+        self.model = do_mpc.model.Model('continuous')
+        self.cA = self.model.set_variable(var_type='_x', var_name='cA', shape=(n_reac,1))
+        self.cB = self.model.set_variable(var_type='_x', var_name='cB', shape=(n_reac,1))
+        self.cR = self.model.set_variable(var_type='_x', var_name='cR', shape=(n_reac,1))
+        self.cS = self.model.set_variable(var_type='_x', var_name='cS', shape=(n_reac,1))
+        self.Tr = self.model.set_variable(var_type='_x', var_name='Tr', shape=(n_reac,1))
+
+        self.uA = self.model.set_variable(var_type='_u', var_name='uA',shape=(n_reac,1))
+        self.uB = self.model.set_variable(var_type='_u', var_name='uB',shape=(n_reac,1))
+        self.Tj = self.model.set_variable(var_type='_u', var_name='Tj',shape=(n_reac,1))
+
+        self.k1=self.model.set_variable(var_type='_tvp', var_name='k1',shape=(n_reac,1))
+        self.k2=self.model.set_variable(var_type='_tvp', var_name='k2',shape=(n_reac,1))
+        self.delH1=self.model.set_variable(var_type='_tvp', var_name='delH1',shape=(n_reac,1))
+        self.delH2=self.model.set_variable(var_type='_tvp', var_name='delH2',shape=(n_reac,1))
+
+
+    def get_model(self, process_noise=False):
+        # Prepare intermediates
+        rhs_cA=[]
+        rhs_cB=[]
+        rhs_cR=[]
+        rhs_cS=[]
+        rhs_Tr=[]
+
+        rhs_cA.append(-self.V_out/self.V_i*self.cA[0]-self.k1[0]*exp(-self.Ea1/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]*self.cB[0]-2*self.k2[0]*exp(-self.Ea2/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]**2+self.uA[0]/self.V_i)
+        rhs_cB.append(-self.V_out/self.V_i*self.cB[0]-self.k1[0]*exp(-self.Ea1/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]*self.cB[0]+self.uB[0]/self.V_i)
+        rhs_cR.append(-self.V_out/self.V_i*self.cR[0]+self.k1[0]*exp(-self.Ea1/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]*self.cB[0])
+        rhs_cS.append(-self.V_out/self.V_i*self.cS[0]+self.k2[0]*exp(-self.Ea2/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]*self.cA[0])
+        rhs_Tr.append(self.V_out/self.V_i*(self.Tr_in-self.Tr[0])+self.kA/(self.rho*self.cp*self.V_i)*(self.Tj[0]-self.Tr[0])-self.delH1[0]/(self.rho*self.cp)*self.k1[0]*exp(-self.Ea1/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]*self.cB[0]-self.delH2[0]/(self.rho*self.cp)*self.k2[0]*exp(-self.Ea2/(self.R_gas*(self.Tr[0]+273.15)))*self.cA[0]*self.cA[0])
+
+        for i in range(1,self.n_reac):
+            rhs_cA.append(self.V_out/self.V_i*(self.cA[i-1]-self.cA[i])-self.k1[i]*exp(-self.Ea1/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]*self.cB[i]-2*self.k2[i]*exp(-self.Ea2/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]**2+self.uA[i]/self.V_i)
+            rhs_cB.append(self.V_out/self.V_i*(self.cB[i-1]-self.cB[i])-self.k1[i]*exp(-self.Ea1/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]*self.cB[i]+self.uB[i]/self.V_i)
+            rhs_cR.append(self.V_out/self.V_i*(self.cR[i-1]-self.cR[i])+self.k1[i]*exp(-self.Ea1/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]*self.cB[i])
+            rhs_cS.append(self.V_out/self.V_i*(self.cS[i-1]-self.cS[i])+self.k2[i]*exp(-self.Ea2/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]*self.cA[i])
+            rhs_Tr.append(self.V_out/self.V_i*(self.Tr[i-1]-self.Tr[i])+self.kA/(self.rho*self.cp*self.V_i)*(self.Tj[i]-self.Tr[i])-self.delH1[i]/(self.rho*self.cp)*self.k1[i]*exp(-self.Ea1/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]*self.cB[i]-self.delH2[i]/(self.rho*self.cp)*self.k2[i]*exp(-self.Ea2/(self.R_gas*(self.Tr[i]+273.15)))*self.cA[i]*self.cA[i])
+        
+        self.model.set_rhs('cA',vertcat(*rhs_cA))
+        self.model.set_rhs('cB', vertcat(*rhs_cB))
+        self.model.set_rhs('cR',vertcat(*rhs_cR))
+        self.model.set_rhs('cS', vertcat(*rhs_cS))
+        self.model.set_rhs('Tr', vertcat(*rhs_Tr))
+
+        self.model.setup()
+
+
+
+
+
+
+
+def print_progress(k,N, bar_len = 50):
+    k = int(max(min(k,N),0))
+    percent_done = round(100*(k)/(N-1))
+
+    done = round(percent_done/(100/bar_len))
+    togo = bar_len-done
+    done_str = '█'*done
+    togo_str = '░'*togo
+
+    msg = f"\t Progress: [{done_str}{togo_str}] {percent_done}% done"
+    print(msg, end='\r')

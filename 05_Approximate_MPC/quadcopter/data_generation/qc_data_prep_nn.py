09cdbca0c2a0b4efe28f8f2f52a4b1daa63de9ba
Felix-Mac <felix.fiedler@tu-berlin.de>
2023-04-24 15:52:59 +0200

    Update data prep

--- 05_Approximate_MPC/quadcopter/data_generation/qc_data_prep_nn.py
+++ 05_Approximate_MPC/quadcopter/data_generation/qc_data_prep_nn.py
@@ -48,32 +48,34 @@ dh.data_dir = os.path.join(data_dir, '')
 
 # %%
 
-dh.set_post_processing('x_k',
-    lambda res: res['sim']['_x']
-)
-dh.set_post_processing('pos_setpoint',
-    lambda res: res['sim']['_p', 'pos_setpoint'])
-dh.set_post_processing('yaw_setpoint',
-    lambda res: res['sim']['_p', 'yaw_setpoint'])
-dh.set_post_processing('u_k',
-    lambda res: res['sim']['_u']
-)
+# dh.set_post_processing('x_k',
+#     lambda res: res['x_k']
+# )
+# dh.set_post_processing('pos_setpoint',
+#     lambda res: res['_p', 'pos_setpoint'])
+# dh.set_post_processing('yaw_setpoint',
+#     lambda res: res['_p', 'yaw_setpoint'])
+# dh.set_post_processing('u_k',
+#     lambda res: res['_u']
+# )
 
+results_with_success = dh.filter(output_filter = lambda res: np.all(res['success']))
 
 # %% [markdown]
 # Stack the results of all trajectories into a single array and create a `pandas` dataframe.
 
-if True:
+if False:
     plt.ion()
     fig, ax = plt.subplots()
 
     lines = []
 
-    for dh_i in dh[:]:
+    for dh_i in results_with_success:
         for i, line in enumerate(lines):
             line[0].set_alpha(1/(i+1))
 
-        lines.append(ax.plot(dh_i['x_k'][:,0], dh_i['x_k'][:,1], color='k'))
+        #lines.append(ax.plot(dh_i['res']['x_k'][:,0], dh_i['res']['x_k'][:,1], color='k'))
+        lines.append(ax.plot(dh_i['res']['pos_k'][:,0], dh_i['res']['pos_k'][:,1], '--', color='r', ))
         plt.show()
         plt.pause(0.5)
 
@@ -85,29 +87,29 @@ if True:
 if True:
     X_K = []
     U_K = []
+    U_K_prev = []
     P_k = []
 
     for res_i in dh[:]:
-        x_k = res_i['x_k']
-        x_k[:,:3] = x_k[:,:3] - res_i['pos_setpoint']
-
-
-        X_K.append(x_k)
-        U_K.append(res_i['u_k'])
-        P_k.append(res_i['yaw_setpoint'])
+        X_K.append(res_i['res']['x_k'][1:,:])
+        U_K.append(res_i['res']['u_k'][1:,:])
+        U_K_prev.append(res_i['res']['u_k'][:-1,:])
+        P_k.append(res_i['res']['p_k'][1:,:])
 
     X_K = np.concatenate(X_K, axis=0)
     U_K = np.concatenate(U_K, axis=0)
+    U_K_prev = np.concatenate(U_K_prev, axis=0)
     P_K = np.concatenate(P_k, axis=0)
 
     data = pd.concat(
         [
             pd.DataFrame(X_K, columns=['dx0', 'dx1', 'dx2', 'v0', 'v1', 'v2', 'phi0', 'phi1', 'phi2', 'omega0', 'omega1', 'omega2']),
             pd.DataFrame(U_K, columns=['f0', 'f1', 'f2', 'f3']),
-            pd.DataFrame(P_K, columns=['yaw_set']),
+            pd.DataFrame(U_K_prev, columns=['f0', 'f1', 'f2', 'f3']),
+            # pd.DataFrame(P_K, columns=['yaw_set']),
         ],
         axis=1,
-        keys=['x_k', 'u_k', 'p_k']
+        keys=['x_k', 'u_k', 'u_k_prev']
     )
 
     # %% [markdown]

fd1ff2ae03741f7973c6cb6f7388295b3139bd19
Felix-Mac <felix.fiedler@tu-berlin.de>
2023-05-03 17:45:57 +0200

    Minor modification in qc_demo

--- 01_Example_Systems/quadcopter/qc_demo.py
+++ 01_Example_Systems/quadcopter/qc_demo.py
@@ -20,7 +20,7 @@ plt.ion()
 
 # %%
 
-t_step = 0.02
+t_step = 0.04
 qcconf = qcmodel.QuadcopterConfig()
 simulator, sim_p_template = qccontrol.get_simulator(t_step, qcmodel.get_model(qcconf, with_pos=True))
 
@@ -32,10 +32,10 @@ mpc.set_initial_guess()
 
 # %%
 
-figure_eight_trajectory = qctrajectory.get_wobbly_figure_eight(s=1.5, a=-1.5, height=1, wobble=0.2, rot=np.pi/2)
+figure_eight_trajectory = qctrajectory.get_wobbly_figure_eight(s=1.5, a=1.5, height=1, wobble=-1, rot=np.pi/2)
 
 res_plot = plot_results.ResultPlot(qcconf, simulator.data, figsize=(12,8))
-simulator.x0['pos'] = np.random.uniform(np.array([-2, -2, 0.]), np.array([2, 2, 1.5]))
+simulator.x0['pos'] = figure_eight_trajectory(0).T[:3]
 simulator.x0['phi'] = np.random.uniform(-np.pi/8*np.ones(3), np.pi/8*np.ones(3))
 simulator.reset_history()
 mpc.reset_history()
@@ -45,7 +45,7 @@ qccontrol.mpc_fly_trajectory(
     mpc, 
     mpc_p_template,
     sim_p_template, 
-    N_iter=200, 
+    N_iter=300, 
     callbacks=[res_plot.draw], 
     trajectory=figure_eight_trajectory,
-    noise_dist=2e-3)
+    noise_dist=2e-2)
